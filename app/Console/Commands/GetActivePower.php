138d62d463b9e2b27f26ffbb5c2d3540c9e2418a
KingSit3 <i.ardiansyah93@gmail.com>
2024-08-31 01:46:28 +0700

    chore: success Save payload to DB

---
+++ app/Console/Commands/GetActivePower.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ActivePower;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use PhpMqtt\Client\MqttClient;
+
+class GetActivePower extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mqtt:active-power';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get new Active Power data';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $result = [];
+
+        $mqtt = new MqttClient(env("MQTT_HOST"), env("MQTT_PORT"), null);
+        $mqtt->connect();
+        $mqtt->subscribe('data/monitor/group3/eon1', function ($topic, $message) use ($mqtt, &$result) {
+            echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
+            $result = json_decode($message, true);
+            $mqtt->interrupt();
+        }, 0);
+
+        $mqtt->loop(true);
+        $mqtt->disconnect();
+
+        ActivePower::create([
+            "terminal_time" => Carbon::parse($result["_terminalTime"])->format('Y-m-d H:i:s'),
+            "active_power_1" => $result["ActivePower1"] ?? null,
+            "active_power_2" => $result["ActivePower2"] ?? null,
+            "active_power_3" => $result["ActivePower3"] ?? null,
+            "active_power_4" => $result["ActivePower4"] ?? null,
+            "active_power_5" => $result["ActivePower5"] ?? null,
+            "active_power_6" => $result["ActivePower6"] ?? null,
+            "active_power_7" => $result["ActivePower7"] ?? null,
+            "active_power_8" => $result["ActivePower8"] ?? null,
+            "active_power_9" => $result["ActivePower9"] ?? null,
+            "active_power_10" => $result["ActivePower10"] ?? null,
+            "active_power_11" => $result["ActivePower11"] ?? null,
+            "active_power_12" => $result["ActivePower12"] ?? null,
+            "active_power_13" => $result["ActivePower17"] ?? null,
+        ]);
+    }
+}
---
+++ database/migrations/2024_08_30_170907_create_active_powers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('active_powers', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->float("active_power_1")->nullable();
+            $table->float("active_power_2")->nullable();
+            $table->float("active_power_3")->nullable();
+            $table->float("active_power_4")->nullable();
+            $table->float("active_power_5")->nullable();
+            $table->float("active_power_6")->nullable();
+            $table->float("active_power_7")->nullable();
+            $table->float("active_power_8")->nullable();
+            $table->float("active_power_9")->nullable();
+            $table->float("active_power_10")->nullable();
+            $table->float("active_power_11")->nullable();
+            $table->float("active_power_12")->nullable();
+            $table->float("active_power_13")->nullable();
+            $table->dateTime("terminal_time");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('active_powers');
+    }
+};
