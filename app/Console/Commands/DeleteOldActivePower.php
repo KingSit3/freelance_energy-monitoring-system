f176190856d0d8470fc6a04bdf4f9d50f734a4d9
KingSit3 <i.ardiansyah93@gmail.com>
2024-08-31 23:26:33 +0700

    chore: cleanup command for Active Power

---
+++ app/Console/Commands/DeleteOldActivePower.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ActivePower;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class GetActivePower extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cleanup:active-power';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete Old Active Power data';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ActivePower::whereDate('created_at', '<', Carbon::today()->subMonths(3)->toDateString())->delete();
+    }
+}
